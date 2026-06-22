28117a53c48e982c0a5d14fd2a46b854cf2ddd26
Bruno Noberto <brunorcnoberto@hotmail.com>
2026-06-22 21:21:44 +0100

    add into the example file the describe: How to find a locator

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -26,3 +26,38 @@ test.describe('suite', () => {
   });
 
 });
+
+test.describe('How to find a locator', () => {
+
+  test('Examples', async ({ page }) => {
+
+    // by tag name
+    page.locator('input')
+
+    // by ID
+    page.locator('#inputEmail1')
+
+    // by Class value
+    page.locator('.shape-rectangle')
+
+    // by attribute
+    page.locator('[placeholder="Email"]')
+
+    // by Class value (full)
+    page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]')
+
+    // by combine differents locators - Add one by one without any blank space
+    page.locator('input[placeholder="Email"]#inputEmail1')
+
+    // by XPath
+    page.locator('//*[id="inputEmail1"]')
+
+    // by particial text
+    page.locator(':text("Using")')
+
+    // by exact text match
+    page.locator(':text-is("Using the Grid")')
+
+  });
+
+})
