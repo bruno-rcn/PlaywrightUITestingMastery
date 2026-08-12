5b6de9737021204436e9d0e0849470484d0bca88
Bruno Noberto <brunorcnoberto@hotmail.com>
2026-08-12 08:26:01 +0100

    Add how to works with Radio buttons and Checkboxes

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -23,5 +23,55 @@ test.describe('Form Layouts page', () => {
         // Assertion
         await expect(theGridInputEmail).toHaveValue('slow@typing.com')
     })
-    
+
+    test('Radio buttons', async ({page}) => {
+        // Radio button is a input filed with propertie type=radio
+
+        // Parent locator
+        const theGridInputEmail = page.locator('nb-card', {hasText: "Using the Grid"})
+
+        // await theGridInputEmail.getByLabel('Option 1').check({force: true}) // use the force because we have a class=visually-hidden and to the check works we need the field visible. When use force true disable PW autowaiting
+        await theGridInputEmail.getByRole('radio', {name: "Option 2"}).check({force: true})
+
+        // Assertion
+        const radioStatus = await theGridInputEmail.getByRole('radio', {name: "Option 2"}).isChecked() // return true or false
+        expect(radioStatus).toBeTruthy() // generic assertion because we are using the boolean return and not the own locator
+        // expect(radioStatus).toBeFalsy()
+
+        await expect(theGridInputEmail.getByLabel('Option 1')).not.toBeChecked()
+        await expect(theGridInputEmail.getByRole('radio', {name: "Option 2"})).toBeChecked()
+    })
+
+})
+
+test.describe('Toastr page', () => {
+
+    test.beforeEach(async ({ page }) => {
+        await page.goto('http://localhost:4200/');
+        await page.getByText('Modal & Overlays').click()
+        await page.getByText('Toastr').click()
+    })
+
+    test('Checkbox', async ({page}) => {
+
+        // If we use click() for checkbox that will change the status. [Checkbox.true - Click() - Checkbox.false] and [Checkbox.false - Click() - Checkbox.true]
+        // If we cares about the status we could use: check() [this way if the checkbox is already checked, nothings happen] and uncheck()
+        await page.getByRole('checkbox', {name: "Hide on click"}).uncheck({force: true})
+        await page.getByRole('checkbox', {name: "Hide on click"}).check({force: true})
+
+        
+        const allBoxes = page.getByRole('checkbox') // work with more than 1 checkbox
+        // uncheck
+        for(const box of await allBoxes.all()){ // we need first to convert allBoxes for an array. Box will interact with any item within the array one by one
+            await box.uncheck({force: true})
+            await expect(box).not.toBeChecked()
+        }
+        // check
+        for(const box of await allBoxes.all()){ 
+            await box.check({force: true})
+            await expect(box).toBeChecked()
+        }
+
+    })
+
 })
